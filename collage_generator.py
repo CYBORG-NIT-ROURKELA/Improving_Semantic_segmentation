997bbf31e3f7440c23e1501029d101c10374f3fd
KSSSenapati <santanusekhar2001.com>
2020-05-03 00:48:33 +0530

    overlay-collag done

--- collage_generator.py
+++ collage_generator.py
@@ -9,9 +9,9 @@ import matplotlib.pyplot as plt
 t = T.Compose([T.Resize(256),
                T.CenterCrop(224)])
 
-rh = np.zeros((224, 10)).astype('int16')
-gh = np.zeros((224, 10)).astype('int16')
-bh = np.zeros((224, 10)).astype('int16')
+rh = np.zeros((3520, 10)).astype('int16')
+gh = np.zeros((3520, 10)).astype('int16')
+bh = np.zeros((3520, 10)).astype('int16')
 
 rh = np.where(rh == 0, 255, 0)
 gh = np.where(gh == 0, 255, 0)
@@ -19,9 +19,9 @@ bh = np.where(bh == 0, 255, 0)
 
 hblank = np.stack([rh, gh, bh], axis=2) 
 
-rv = np.zeros((10, 692)).astype('int16')
-gv = np.zeros((10, 692)).astype('int16')
-bv = np.zeros((10, 692)).astype('int16')
+rv = np.zeros((10, 224)).astype('int16')
+gv = np.zeros((10, 224)).astype('int16')
+bv = np.zeros((10, 224)).astype('int16')
 
 rv = np.where(rv == 0, 255, 0)
 gv = np.where(gv == 0, 255, 0)
@@ -44,8 +44,16 @@ for i in range(n):
 
 image = vblank
 for i in range(len(rows)):
-    image = np.vstack((image, rows[0], vblank))
+    image = np.vstack((image, rows[i], vblank))
+
+image2 = vblank
+for i in range(n):
+    img = Image.open(images_dir + images[i])
+    img = t(img)
+    image2 = np.vstack((image2, img, vblank))
     
+overlay_image = np.hstack((image2, hblank, (image * 255).astype(np.uint8)))
+
 cv2.imwrite("overlay_collage_2nd_part.jpg", image)
 temp = cv2.imread(image)
 cv2.imshow("abc", image)
