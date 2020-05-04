733b496a67f96b371b5a6c9d27040dfcac79d1dd
KSSSenapati <santanusekhar2001.com>
2020-05-04 12:57:49 +0530

    images selection

---
+++ select.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May  3 22:00:32 2020
+
+@author: Asus
+"""
+number = []
+for i in range(n):
+    if max(miou_deeplab[i], miou_fz[i], miou_slic[i], miou_quick[i], miou_watershed[i]) == miou_quick[i]:
+        number.append(i)
