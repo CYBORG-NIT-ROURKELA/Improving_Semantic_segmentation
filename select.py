75876aad149100449052a985bba4b50b1a580377
KSSSenapati <santanusekhar2001.com>
2020-05-06 23:42:31 +0530

    change

--- select.py
+++ select.py
@@ -8,3 +8,4 @@ number = []
 for i in range(n):
     if max(miou_deeplab[i], miou_fz[i], miou_slic[i], miou_quick[i], miou_watershed[i]) == miou_quick[i]:
         number.append(i)
+print("no. of cases where quick-shift clustering improves the result", len(number))
